5981a410d795ce51cd00f83955e2663387d7c257
Louay Morsi <louay.hesham@gmail.com>
2018-10-27 13:52:31 +0200

    Added main multiplication logic

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int** readMatrix(int *n, int *m) {
     cin >> *n;
     cin >> *m;
-    int **matrix;
+    int **matrix = new int*[*n];
 
     for (int i = 0; i < *n; i++) {
         matrix[i] = new int[*m];
@@ -16,8 +16,39 @@ int** readMatrix(int *n, int *m) {
     return matrix;
 }
 
-void matrixMultiplyElement(int n1, int m1, int** matrix1, int n2, int m2, int** matrix2) {
+int** transpose(int n, int m, int** matrix) {
+    int **matrixTransposed = new int*[m];
+    for (int j = 0; j < m; j++) {
+        matrixTransposed[j] = new int[n];
+        for (int i = 0; i < n; i++) {
+            matrixTransposed[j][i] = matrix[i][j];
+        }
+    }
+    return matrixTransposed;
+}
 
+void compute_element(int n, int* row, int* column, int* result_location) {
+    int element = 0;
+    for (int i = 0; i < n; i++) {
+        element += (row[i] * column[i]);
+    }
+    *result_location = element;
+}
+
+void matrixMultiplyElement(int n1, int m1, int** matrix1, int n2, int m2, int** matrix2) {
+    int** matrix2_transposed = transpose(n2, m2, matrix2);
+    int result_matrix[n1][m2];
+    for (int i =0; i < n1; i++) {
+        for (int j = 0; j < m2; j++) {
+            compute_element(n2, matrix1[i], matrix2_transposed[j], &result_matrix[i][j]);
+        }
+    }
+    for (int i =0; i < n1; i++) {
+        for (int j = 0; j < m2; j++) {
+            cout << result_matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
 }
 
 void matrixMultiplyRow() {
@@ -39,7 +70,7 @@ void mergeSort() {
 }
 
 int main() {
-    while (1) {
+    //while (1) {
         cout << "Choose option:" << endl;
         cout << "1- Matrix multiplication" << endl;
         cout << "2- Merge sort" << endl;
@@ -58,6 +89,6 @@ int main() {
         default:
             cout << "Incorrect option" << endl;
         }
-    }
+    //}
     return 0;
 }
