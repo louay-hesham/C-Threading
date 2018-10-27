cead8a5eb9110d38fe68baa1e977fdbcc12072a1
Louay Morsi <louay.hesham@gmail.com>
2018-10-27 13:15:51 +0200

    Reading from file

--- main.cpp
+++ main.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 
-void matrixMultiplyElement() {
+int** readMatrix(int *n, int *m) {
+    cin >> *n;
+    cin >> *m;
+    int **matrix;
+
+    for (int i = 0; i < *n; i++) {
+        matrix[i] = new int[*m];
+        for (int j = 0; j < *m; j++)
+            cin >> matrix[i][j];
+    }
+    return matrix;
+}
+
+void matrixMultiplyElement(int n1, int m1, int** matrix1, int n2, int m2, int** matrix2) {
 
 }
 
 void matrixMultiplyRow() {
 
+}
+
+void matrixMultiply() {
+    freopen("input.txt","r",stdin);
+    int n1, m1;
+    int** matrix1 = readMatrix(&n1, &m1);
+    int n2, m2;
+    int** matrix2 = readMatrix(&n2, &m2);
+    matrixMultiplyElement(n1, m1, matrix1, n2, m2, matrix2);
+    matrixMultiplyRow();
 }
 
 void mergeSort() {
@@ -17,9 +41,8 @@ void mergeSort() {
 int main() {
     while (1) {
         cout << "Choose option:" << endl;
-        cout << "1- Matrix multiplication (one element per thread)" << endl;
-        cout << "2- Matrix multiplication (one row per thread)" << endl;
-        cout << "3- Merge sort" << endl;
+        cout << "1- Matrix multiplication" << endl;
+        cout << "2- Merge sort" << endl;
         cout << "0- Exit" << endl;
         int choice = -1;
         cin >> choice;
@@ -27,11 +50,11 @@ int main() {
         case 0:
             return 0;
         case 1:
-            matrixMultiplyElement();
+            matrixMultiply();
+            break;
         case 2:
-            matrixMultiplyRow();
-        case 3:
             mergeSort();
+            break;
         default:
             cout << "Incorrect option" << endl;
         }
